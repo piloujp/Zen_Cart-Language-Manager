4d7206b1f02d29a2ca2ba1910ca37dd718a803e4
Leonard <info@zenexpert.com>
2026-01-05 11:51:38 +0100

    fixes #1

--- zc_plugins/LanguageManager/1.0.0/manifest.php
+++ zc_plugins/LanguageManager/1.0.0/manifest.php
@@ -5,7 +5,7 @@ return [
     'pluginName' => 'Language Manager',
     'pluginDescription' => 'Allows easy management of language definitions directly from the admin interface, including bulk updates and advanced editing features.',
     'pluginAuthor' => 'ZenExpert',
-    'pluginId' => '', // ID from Zen Cart forum
+    'pluginId' => 0, // ID from Zen Cart forum
     'zcVersions' => ['v2.0.0', 'v2.0.1', 'v2.1.0'], // compatible versions of Zen Cart
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => 'https://github.com/zenexpert/Zen_Cart-Language-Manager', // url
